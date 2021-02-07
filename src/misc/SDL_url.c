0317ae21337f428afcccff85e8215f77fe1d8c8c
Steven Noonan <steven@uplinklabs.net>
2021-02-07 06:41:42 -0800

    add build system for Meson, Makefiles, and MSVC
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/misc/SDL_url.c
+++ src/misc/SDL_url.c
@@ -19,6 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+#include "SDL_internal.h"
 #include "SDL_sysurl.h"
 
 extern int SDL_SYS_OpenURL(const char *url);
--- src/render/SDL_yuv_sw_c.h
+++ src/render/SDL_yuv_sw_c.h
@@ -22,7 +22,7 @@
 #ifndef SDL_yuv_sw_c_h_
 #define SDL_yuv_sw_c_h_
 
-#include "../SDL_internal.h"
+#include "SDL_internal.h"
 
 #include "SDL_video.h"
 
