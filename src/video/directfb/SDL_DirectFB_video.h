f44795d4bde99e9122e311130a087dab9e48cd61
Steven Noonan <steven@uplinklabs.net>
2021-02-07 06:41:42 -0800

    build: add build system for Meson, Makefiles, and MSVC
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/video/directfb/SDL_DirectFB_video.h
+++ src/video/directfb/SDL_DirectFB_video.h
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-#include "../../SDL_internal.h"
+#include "SDL_internal.h"
 
 #ifndef SDL_DirectFB_video_h_
 #define SDL_DirectFB_video_h_
