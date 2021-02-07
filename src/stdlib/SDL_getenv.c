74197d92b742ce7ad6d5e7fda2ca146aba033efa
Steven Noonan <steven@uplinklabs.net>
2021-02-07 06:41:42 -0800

    add build system for Meson, Makefiles, and MSVC
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/stdlib/SDL_getenv.c
+++ src/stdlib/SDL_getenv.c
@@ -19,11 +19,11 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+#include "SDL_internal.h"
+
 #if defined(__clang_analyzer__) && !defined(SDL_DISABLE_ANALYZE_MACROS)
 #define SDL_DISABLE_ANALYZE_MACROS 1
 #endif
-
-#include "../SDL_internal.h"
 
 #if defined(__WIN32__)
 #include "../core/windows/SDL_windows.h"
