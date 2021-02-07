a30e8d4a46f481f5baaa7e8481aa8db65e904762
Steven Noonan <steven@uplinklabs.net>
2021-02-07 06:41:42 -0800

    add build system for Meson, Makefiles, and MSVC
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/video/wayland/SDL_waylandvideo.h
+++ src/video/wayland/SDL_waylandvideo.h
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-#include "../../SDL_internal.h"
+#include "SDL_internal.h"
 #include "SDL_stdinc.h"
 
 #ifndef SDL_waylandvideo_h_
