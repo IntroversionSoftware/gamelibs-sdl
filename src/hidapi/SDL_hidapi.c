ad7564fcedc0c8dec901dec076ecaf70135c36d3
Steven Noonan <steven@uplinklabs.net>
2021-02-07 06:39:48 -0800

    SDL_hidapi.c: fix SDL_udev.h include path
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/hidapi/SDL_hidapi.c
+++ src/hidapi/SDL_hidapi.c
@@ -550,7 +550,7 @@ HIDAPI_ShutdownDiscovery()
 #undef HIDAPI_H__
 #if __LINUX__
 
-#include "../../core/linux/SDL_udev.h"
+#include "../core/linux/SDL_udev.h"
 #if SDL_USE_LIBUDEV
 static const SDL_UDEV_Symbols *udev_ctx = NULL;
 
