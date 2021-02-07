29c6f541a60d978810a8d0403c4fcd858ba566d1
Steven Noonan <steven@uplinklabs.net>
2021-02-07 06:41:42 -0800

    add build system for Meson, Makefiles, and MSVC
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/joystick/windows/SDL_dinputjoystick_c.h
+++ src/joystick/windows/SDL_dinputjoystick_c.h
@@ -18,7 +18,7 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 */
-#include "../../SDL_internal.h"
+#include "SDL_internal.h"
 
 extern int SDL_DINPUT_JoystickInit(void);
 extern void SDL_DINPUT_JoystickDetect(JoyStick_DeviceData **pContext);
