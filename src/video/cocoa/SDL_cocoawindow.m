0d58796bc76b1dd0bb7a1276379b164ee1f0f389
Steven Noonan <steven@uplinklabs.net>
2021-04-09 08:23:36 -0700

    cocoa: mark window as opaque content
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/video/cocoa/SDL_cocoawindow.m
+++ src/video/cocoa/SDL_cocoawindow.m
@@ -63,6 +63,7 @@
 /* These are needed for borderless/fullscreen windows */
 - (BOOL)canBecomeKeyWindow;
 - (BOOL)canBecomeMainWindow;
+- (BOOL)isOpaque;
 - (void)sendEvent:(NSEvent *)event;
 - (void)doCommandBySelector:(SEL)aSelector;
 
@@ -101,6 +102,11 @@
 }
 
 - (BOOL)canBecomeMainWindow
+{
+    return YES;
+}
+
+- (BOOL)isOpaqua
 {
     return YES;
 }
