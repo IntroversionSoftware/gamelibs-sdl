713545aa13e7e4c04d3968ebcb3cb03c69431ab4
Steven Noonan <steven@uplinklabs.net>
2021-09-18 01:41:16 -0700

    SDL_render: silence a clang-cl warning about an unused function
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/render/SDL_render.c
+++ src/render/SDL_render.c
@@ -835,6 +835,7 @@ SDL_CreateWindowAndRenderer(int width, int height, Uint32 window_flags,
     return 0;
 }
 
+#if !SDL_RENDER_DISABLED
 static SDL_INLINE
 void VerifyDrawQueueFunctions(const SDL_Renderer *renderer)
 {
@@ -848,6 +849,7 @@ void VerifyDrawQueueFunctions(const SDL_Renderer *renderer)
     SDL_assert(renderer->QueueCopy != NULL);
     SDL_assert(renderer->RunCommandQueue != NULL);
 }
+#endif
 
 SDL_Renderer *
 SDL_CreateRenderer(SDL_Window * window, int index, Uint32 flags)
