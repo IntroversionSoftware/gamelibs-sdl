e46435c545b5a545019191838a6fc1f3704fe155
Steven Noonan <steven@valvesoftware.com>
2022-08-25 20:29:41 -0700

    egl: implement SDL_GL_EGL_PLATFORM attribute
    This implements a new SDL_GL_EGL_PLATFORM attribute to set the
    "platform" argument for SDL_EGL_LoadLibrary on Windows, macOS, and
    Linux. I've limited it to those three operating systems because that's
    what I've been able to test against.

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -3645,6 +3645,8 @@ SDL_GL_ResetAttributes()
     _this->gl_config.reset_notification = SDL_GL_CONTEXT_RESET_NO_NOTIFICATION;
 
     _this->gl_config.share_with_current_context = 0;
+
+    _this->gl_config.egl_platform = 0;
 }
 
 int
@@ -3760,6 +3762,9 @@ SDL_GL_SetAttribute(SDL_GLattr attr, int value)
         break;
     case SDL_GL_CONTEXT_NO_ERROR:
         _this->gl_config.no_error = value;
+        break;
+    case SDL_GL_EGL_PLATFORM:
+        _this->gl_config.egl_platform = value;
         break;
     default:
         retval = SDL_SetError("Unknown OpenGL attribute");
@@ -3970,6 +3975,12 @@ SDL_GL_GetAttribute(SDL_GLattr attr, int *value)
             *value = _this->gl_config.no_error;
             return 0;
         }
+    case SDL_GL_EGL_PLATFORM:
+        {
+            *value = _this->gl_config.egl_platform;
+            return 0;
+        }
+        break;
     default:
         return SDL_SetError("Unknown OpenGL attribute");
     }
