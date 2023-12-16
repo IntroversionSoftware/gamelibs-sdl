4f22968a6b49bae8fa1e647627e978fd9cfe17db
Steven Noonan <steven@uplinklabs.net>
2023-12-16 00:54:58 -0800

    add an 'is_native' field to SDL_DisplayMode
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/video/wayland/SDL_waylandvideo.c
+++ src/video/wayland/SDL_waylandvideo.c
@@ -593,6 +593,7 @@ static void display_handle_done(void *data,
         native_mode.w = driverdata->native_width;
         native_mode.h = driverdata->native_height;
     }
+    native_mode.is_native = 1;
     native_mode.refresh_rate = (int)SDL_round(driverdata->refresh / 1000.0); /* mHz to Hz */
 
     /* The scaled desktop mode */
@@ -614,6 +615,7 @@ static void display_handle_done(void *data,
         desktop_mode.w = driverdata->height;
         desktop_mode.h = driverdata->width;
     }
+    desktop_mode.is_native = 1;
     desktop_mode.refresh_rate = (int)SDL_round(driverdata->refresh / 1000.0); /* mHz to Hz */
 
     /*
