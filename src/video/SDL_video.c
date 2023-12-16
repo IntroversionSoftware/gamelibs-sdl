94ca0435d87a62c9311ae5a67090aeebe8a52d11
Steven Noonan <steven@uplinklabs.net>
2023-12-16 00:54:56 -0800

    SDL_video: set max ES profile to 3.2 always
    Windows AMD drivers don't report support for GL_ARB_ES3_*_compatibility,
    but they do support all the way through 3.2.
    
    Signed-off-by: Steven Noonan <steven@valvesoftware.com>

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -4850,6 +4850,7 @@ void SDL_GL_DeduceMaxSupportedESProfile(int *major, int *minor)
     /* XXX This is fragile; it will break in the event of release of
      * new versions of OpenGL ES.
      */
+    #if 0
     if (SDL_GL_ExtensionSupported("GL_ARB_ES3_2_compatibility")) {
         *major = 3;
         *minor = 2;
@@ -4863,6 +4864,9 @@ void SDL_GL_DeduceMaxSupportedESProfile(int *major, int *minor)
         *major = 2;
         *minor = 0;
     }
+    #endif
+    *major = 3;
+    *minor = 2;
 #endif
 }
 
