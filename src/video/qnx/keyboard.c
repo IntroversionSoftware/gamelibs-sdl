52b198aff7dd7f268763e68339e416a495cd4dc8
Steven Noonan <steven@uplinklabs.net>
2023-12-16 00:54:54 -0800

    build: add build system for Meson, Makefiles, and MSVC
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/video/qnx/keyboard.c
+++ src/video/qnx/keyboard.c
@@ -19,7 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
-#include "../../SDL_internal.h"
+#include "SDL_internal.h"
 #include "../../events/SDL_keyboard_c.h"
 #include "SDL_scancode.h"
 #include "SDL_events.h"
