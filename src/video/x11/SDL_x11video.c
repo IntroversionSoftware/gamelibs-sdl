fc299b5bd8d2a5bd963ef9d67e25857e44f04aa7
Steven Noonan <steven@uplinklabs.net>
2023-12-16 00:55:08 -0800

    hints: replace SDL_HINT_VIDEO_X11_FORCE_EGL with platform-agnostic SDL_HINT_VIDEO_FORCE_EGL
    This adds support for forcing the use of EGL on Windows and MacOS. The
    SDL_HINT_VIDEO_X11_FORCE_EGL hint is retained for backwards
    compatibility but is now deprecated.

--- src/video/x11/SDL_x11video.c
+++ src/video/x11/SDL_x11video.c
@@ -289,7 +289,8 @@ static SDL_VideoDevice *X11_CreateDevice(void)
 #endif
 #ifdef SDL_VIDEO_OPENGL_EGL
 #ifdef SDL_VIDEO_OPENGL_GLX
-    if (SDL_GetHintBoolean(SDL_HINT_VIDEO_X11_FORCE_EGL, SDL_FALSE)) {
+    if (SDL_GetHintBoolean(SDL_HINT_VIDEO_FORCE_EGL, SDL_FALSE) ||
+        SDL_GetHintBoolean(SDL_HINT_VIDEO_X11_FORCE_EGL, SDL_FALSE)) {
 #endif
         device->GL_LoadLibrary = X11_GLES_LoadLibrary;
         device->GL_GetProcAddress = X11_GLES_GetProcAddress;
