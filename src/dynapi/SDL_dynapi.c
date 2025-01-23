e4b58b23747683e44952f1b80a0d7d1413173a96
Steven Noonan <steven@uplinklabs.net>
2025-01-22 20:51:44 -0800

    windows: fix missing PCH for some .c files
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/dynapi/SDL_dynapi.c
+++ src/dynapi/SDL_dynapi.c
@@ -19,6 +19,7 @@
   3. This notice may not be removed or altered from any source distribution.
 */
 
+#include "SDL_internal.h"
 #include "SDL_build_config.h"
 #include "SDL_dynapi.h"
 #include "SDL_dynapi_unsupported.h"
