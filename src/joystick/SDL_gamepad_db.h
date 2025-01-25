7114bc7a9c2c1ea2f36e3aee9dbb63762ad1f779
Steven Noonan <steven@uplinklabs.net>
2025-01-25 08:48:55 -0800

    SDL_gamepad_db: disable, our game provides its own external mappings list
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/joystick/SDL_gamepad_db.h
+++ src/joystick/SDL_gamepad_db.h
@@ -29,6 +29,7 @@
    Alternatively, you can use the app located in test/controllermap
  */
 static const char *s_GamepadMappings[] = {
+#if 0
 #ifdef SDL_JOYSTICK_PRIVATE
     SDL_PRIVATE_GAMEPAD_DEFINITIONS
 #endif
@@ -894,5 +895,6 @@ static const char *s_GamepadMappings[] = {
     "000000004e696e74656e646f20334400,Nintendo 3DS,crc:3210,a:b0,b:b1,back:b2,dpdown:b7,dpleft:b5,dpright:b4,dpup:b6,leftshoulder:b9,lefttrigger:b14,leftx:a0,lefty:a1,rightshoulder:b8,righttrigger:b15,rightx:a2,righty:a3,start:b3,x:b10,y:b11,",
 #endif
     "hidapi,*,a:b0,b:b1,back:b4,dpdown:b12,dpleft:b13,dpright:b14,dpup:b11,guide:b5,leftshoulder:b9,leftstick:b7,lefttrigger:a4,leftx:a0,lefty:a1,rightshoulder:b10,rightstick:b8,righttrigger:a5,rightx:a2,righty:a3,start:b6,x:b2,y:b3,",
+#endif
     NULL
 };
