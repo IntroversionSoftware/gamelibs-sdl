98ee24c088f8b2d8f278389be2febda33f903844
Steven Noonan <steven@uplinklabs.net>
2025-02-11 01:56:34 -0800

    SDL_events: send wakeup event on RunOnMainThread wait
    This resolves some very serious latency issues with RunOnMainThread.
    
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/events/SDL_events.c
+++ src/events/SDL_events.c
@@ -1459,6 +1459,8 @@ bool SDL_RunOnMainThread(SDL_MainThreadCallback callback, void *userdata, bool w
         return true;
     }
 
+    SDL_SendWakeupEvent();
+
     SDL_WaitSemaphore(entry->semaphore);
 
     switch (SDL_GetAtomicInt(&entry->state)) {
